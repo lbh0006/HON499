c9aa485f372bee427e93bd75088f04b3eaf6f18d
lbh0006 <lbh0006@uah.edu>
2016-10-19 17:17:10 -0500

    attempted to update GUI and thesis documentation. GUI is broken.

--- StoryGenerator/src/StoryGeneratorGUI/GUIWindow.java
+++ StoryGenerator/src/StoryGeneratorGUI/GUIWindow.java
@@ -6,17 +6,13 @@
 package StoryGeneratorGUI;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.Frame;
-import java.awt.Graphics2D;
 import java.awt.GridLayout;
-import java.awt.image.BufferedImage;
-import java.util.Calendar;
-import java.util.Date;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
-import sun.util.calendar.CalendarDate;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  *
@@ -25,73 +21,77 @@ import sun.util.calendar.CalendarDate;
 
 // GUI - full screen window
 public final class GUIWindow extends JFrame{
-    JPanel jpegPanel, btnPanel1, btnPanel2, datePanel, timePanel, latPanel, longPanel;
-    JScrollPane skyMapScrollPane;
-    JButton saveBtn, generateMapBtn;
-    JTextField dayField, monthField, yearField, latField, longField;
-    JSpinner hourSpinner, minuteSpinner, secondSpinner;
-    SpinnerDateModel hourModel, minuteModel, secondModel;
-    JLabel dateLbl, latLbl, longLbl, backslashLbl1, backslashLbl2;
+    JPanel btnPanel,textPanel;
+    JScrollPane scrollPane;
+    JButton saveBtn, generateBtn;
+    JTextField story;
+    String storyText;
     
     public GUIWindow() {
-        setExtendedState(Frame.MAXIMIZED_BOTH);
-        btnPanel1 = new JPanel();
-        jpegPanel = new JPanel();
-        btnPanel2 = new JPanel();
-        datePanel = new JPanel();
-        timePanel = new JPanel(); 
-
-        minuteSpinner = new JSpinner(new SpinnerDateModel(new Date(), null, null, Calendar.YEAR));
-        secondSpinner = new JSpinner();
-
+        //storyText = StoryGenerator.storyText;
+        story = new JTextField(storyText);
+        textPanel = new JPanel();
+        textPanel.setLayout(new GridLayout(1,1,1,1));
+        textPanel.add(story);
+        btnPanel = new JPanel();
+        generateBtn = new JButton("Generate");
         saveBtn = new JButton("Save");
-        generateMapBtn = new JButton("Generate Map");
-        dayField = new JTextField("DD");
-        monthField = new JTextField("MM");
-        yearField = new JTextField("YYYY");
-        latField = new JTextField("0");
-        longField = new JTextField("0");
-        dateLbl = new JLabel("Date: ");
-        latLbl = new JLabel("Lat: ");
-        longLbl =  new JLabel("Long: ");
-        backslashLbl1 = new JLabel("/");  
-        backslashLbl2 = new JLabel("/");
-        latPanel = new JPanel();
-        longPanel = new JPanel();
-        datePanel.add(dateLbl);
-        datePanel.add(dayField);
-        datePanel.add(backslashLbl1);
-        datePanel.add(monthField);
-        datePanel.add(backslashLbl2);
-        datePanel.add(yearField);
-        latPanel.add(latLbl);
-        latPanel.add(latField);
-        longPanel.add(longLbl);
-        longPanel.add(longField);
-        skyMapScrollPane = new JScrollPane(jpegPanel);
-        skyMapScrollPane.setPreferredSize(new Dimension(100, 100));
-        setTitle("SkyMap");
+        scrollPane = new JScrollPane(textPanel);
+        scrollPane.setPreferredSize(new Dimension(100, 100));
+        
+        setTitle("Story Generator");
         setMinimumSize(new Dimension(500, 500));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-        setLayout(new BorderLayout());        
-        jpegPanel.setMinimumSize(new Dimension(800, 800));
-        btnPanel1.setLayout(new GridLayout(2, 2, 1, 1));        
-        btnPanel1.setPreferredSize(new Dimension(50, 75));
-        btnPanel1.add(datePanel);
+        setLayout(new BorderLayout());
+        btnPanel.setLayout(new GridLayout(2, 1, 1, 1));
+        btnPanel.add(generateBtn);
+        btnPanel.add(saveBtn);
+        add(scrollPane, BorderLayout.CENTER);
         
-        btnPanel1.add(latPanel);
-        btnPanel1.add(longPanel);
-        btnPanel2.setLayout(new GridLayout(2, 1, 1, 1));
-        btnPanel2.add(generateMapBtn);
-        btnPanel2.add(saveBtn);
-        add(skyMapScrollPane, BorderLayout.CENTER);
         JPanel mainBtnPanel = new JPanel();
         mainBtnPanel.setLayout(new GridLayout(1, 2, 1, 1));
-        mainBtnPanel.add(btnPanel1);
         mainBtnPanel.add(new JPanel());
-        mainBtnPanel.add(btnPanel2);
-        add(mainBtnPanel, BorderLayout.SOUTH);        
+        mainBtnPanel.add(btnPanel);
+        mainBtnPanel.add(new JPanel());
+        add(mainBtnPanel, BorderLayout.SOUTH); 
+    }
+    
+    public void addListeners() {
+        generateBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                story.setText("Generate");
+                
+            }
+        });
         
+        //action listener for the save button
+        saveBtn.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                story.setText("Save");
+                //get a file chooser, add a filter for txt only
+                JFileChooser c = new JFileChooser();
+                FileFilter filter = new FileNameExtensionFilter("txt text files", "txt");
+                c.setDialogTitle("Save As");
+                c.setFileFilter(filter);
+
+                //if user enters a correct filename
+                int saveValue = c.showSaveDialog(GUIWindow.this);
+                if (saveValue == JFileChooser.APPROVE_OPTION) {
+                    //Test the string for .txt extention
+                    String fileName = (c.getSelectedFile().toString());
+                    if (fileName.contains(".txt") == false) {
+                        fileName += ".txt";
+                    }
+
+                    //get new instance of AstroDraw and create the offscreen image
+                    //AstroDraw ad = new AstroDraw();
+                    //ad.createOffScreenImage(fileName);
+                }
+            }
+        });
     }
 }
--- StoryGenerator/src/storygenerator/StoryGenerator.java
+++ StoryGenerator/src/storygenerator/StoryGenerator.java
@@ -9,7 +9,6 @@ import StoryGeneratorGUI.GUIWindow;
  *
  * @author Lindsey Harris
  */
-
 public class StoryGenerator {
     ProbabilityTree<Scene> sceneMap;
     Character protagonist;
@@ -19,12 +18,14 @@ public class StoryGenerator {
     }
     
     public static void main(String[] args) {
-        GUIWindow gui = new GUIWindow();
+        
         ApplicationTester t = new ApplicationTester();
         t.initialTestOfAdvProbabilityTree(0.3, 0.3, 0.3, 
                                           0.0002, 1.0,
                                           0.0002, 1.0,
                                           0.0002, 1.0);
+        
+        GUIWindow gui = new GUIWindow();
     }
     
     
